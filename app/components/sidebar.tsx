4bb9dc25602db982e384dedda48cbcfeb32929fa
charlotte076 <124852996+charlotte076@users.noreply.github.com>
2023-07-11 02:55:31 +0000

    fix: test

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,7 +117,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          Charlotte
         </div>
         <div className={styles["sidebar-sub-title"]}>不与奈绪说再见</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
